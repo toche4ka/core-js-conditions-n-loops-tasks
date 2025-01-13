bc9e8595717f0db530a7a35d6d5db122c1e84cbe
Anna Tochk <anitochka1105@gmail.com>
2025-01-13 05:30:38 +0500

    feat: add solutions

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -173,8 +173,12 @@ function convertNumberToString(/* numberStr */) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  for (let i = 0; i < str.length / 2; i += 1)
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  return true;
 }
 
 /**
@@ -191,8 +195,12 @@ function isPalindrome(/* str */) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  for (let i = 0; i < str.length; i += 1)
+    if (str[i] === letter) {
+      return i;
+    }
+  return -1;
 }
 
 /**
